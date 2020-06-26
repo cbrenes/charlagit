64ac0718b1603aec2d434a638bf0017487abc9b2
Cesar <cesarbresola@gmail.com>
2020-06-25 18:02:11 -0600

    agregado el proyecto inicial

---
+++ CharlaExample/CharlaExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CharlaExample
+//
+//  Created by Cesar Brenes on 6/25/20.
+//  Copyright © 2020 VeuxLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
