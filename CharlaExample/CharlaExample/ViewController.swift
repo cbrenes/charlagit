44ba73a1f4667e4e770719a9bc1c1140cfbfd21d
Cesar <cesarbresola@gmail.com>
2020-06-25 18:08:25 -0600

    commit example

--- CharlaExample/CharlaExample/ViewController.swift
+++ CharlaExample/CharlaExample/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let test = "string test"
+    let test2 = "string test"
+    let test3 = "string test"
+    let test4 = "string test"
+    let tes5 = "string test"
+    let test6 = "string test"
+    let tes7 = "string test"
+    let test8 = "string test"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
