a6905507145b5404d7abf59e6db855253e5fca03
Cesar <cesarbresola@gmail.com>
2020-06-25 18:10:34 -0600

    second commit

--- CharlaExample/CharlaExample/ViewController.swift
+++ CharlaExample/CharlaExample/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let number1 = 1
+    let number2 = 2
+    let number3 = 3
+    let number4 = 4
+    let number5 = 5
+    let number6 = 6
+    let number7 = 7
+    let number8 = 8
+    let number9 = 9
+    let number10 = 10
+    let number11 = 11
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
